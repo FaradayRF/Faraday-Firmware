3df0893484b449a5b187d0474c2272f56b528f34
Brenton Salmi <brent@faradayrf.com>
2016-10-11 01:40:07 -0700

    Added FLASH Library
    Added basic support for FLASH library. No working operations yet but
    laying out the framework and including documentation from the start.

---
+++ FLASH/FLASH.c
@@ -0,0 +1,37 @@
+/** @file FLASH.c
+ *  @brief Hardware abstraction for the onboard Faraday FLASH memory IC.
+ *
+ *  The functions below provide input/output ability with the onboard
+ *  FLASH memory IC using SPI. Read and Write functioanlity is supportted.
+ *
+ */
+
+/* -- Includes -- */
+
+/* standard includes */
+#include <msp430.h>
+
+/* faraday hardware allocations */
+#include "../../REVA_Faraday.h"
+
+
+
+void Faraday_FLASH_CE_Enable(void){
+	//Active LOW
+	P3OUT &= ~FLASH_SPI_CS;
+}
+
+void Faraday_FLASH_CE_Disable(void){
+	//Active LOW
+	P3OUT |= FLASH_SPI_CS;
+}
+
+void Faraday_FLASH_Hold_Enable(void){
+	//Active LOW
+	P5OUT &= ~FLASH_HOLD;
+}
+
+void Faraday_FLASH_Hold_Disable(void){
+	//Active LOW
+	P5OUT |= FLASH_HOLD;
+}
