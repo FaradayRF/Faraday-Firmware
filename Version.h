3c123bbb98939d8ce56720b66585c271d4590c67
Brenton Salmi <brent@faradayrf.com>
2017-02-10 00:59:16 -0800

    Adding basic firmware revision
    Header file containing long variable.

---
+++ Version.h
@@ -0,0 +1,22 @@
+/** @file Version.h
+ *  @brief Header file that contains firmware revision information
+ *
+ *  This files contains information used to determine firmware
+ *  revision identity.
+ *
+ */
+
+/** @name Firmware Revision Constants
+*
+* 	Constants used for the firmware revision identification
+*
+@{**/
+#define FIRMWARE_REVISION_SIZE 4 /**< Firmware revision is size long*/
+/** @}*/
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+const unsigned long firmware_revision = 1; //
+
+#endif /* VERSION_H_ */
